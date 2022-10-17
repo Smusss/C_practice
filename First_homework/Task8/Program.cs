1a7286dd4cab5c03c85e10e5cfc9868dc27020df
Smusss <lilly_dallas@bk.ru>
2022-10-17 18:51:24 +0300

    Correct Task 8 optim

--- First_homework/Task8/Program.cs
+++ First_homework/Task8/Program.cs
@@ -2,10 +2,10 @@
 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
 Console.Write("Введите число N ");
 int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Четные числа от 1 до N: ");
 if (N > 0)
 {
 int evenN = 2;
-Console.Write("Четные числа от 1 до N: ");
 while (evenN <= N)
 {
     Console.Write(evenN + " ");
