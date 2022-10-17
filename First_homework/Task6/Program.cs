f25796e1a9bdb966f6a3d859caa00737affd8995
Smusss <lilly_dallas@bk.ru>
2022-10-17 17:11:56 +0300

    Correct Task 6

--- First_homework/Task6/Program.cs
+++ First_homework/Task6/Program.cs
@@ -1,6 +1,16 @@
 ﻿/*   An even or odd
 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).*/
 Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+   Console.Write("Число " + number + " четное"); 
+}
+else
+{
+    Console.Write("Число " + number + " нечетное"); 
+}
+/* решила попробовать применить функцию:
 Int64 number = Convert.ToInt64(Console.ReadLine());
 Int64 result = 0;
 Math.DivRem(number,2,out result);
@@ -12,3 +22,4 @@ else
 {
     Console.Write("Число " + number + " нечетное"); 
 }
+*/
