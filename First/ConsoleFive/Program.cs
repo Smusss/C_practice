504b9375321d97e68a3adb91716a96f8b35a9411
Smusss <lilly_dallas@bk.ru>
2022-10-17 20:39:57 +0300

    Add ConsoleFive

---
+++ First/ConsoleFive/Program.cs
@@ -0,0 +1,15 @@
+﻿/*The last number in three-digit number
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Console.ReadLine();
+var last = 
+if (number > 999 | number < 100)
+{
+    Console.WriteLine("Значение не соответствет запросу, попробуйте снова.");
+}
+else
+{
+     Console.WriteLine(inputLine.last(num));
+}
