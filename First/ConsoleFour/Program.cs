08990d3e5b06d850f73ada2e8c15f3cdc54e39ed
Smusss <lilly_dallas@bk.ru>
2022-10-17 20:09:40 +0300

    Add ConsoleFour

---
+++ First/ConsoleFour/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Range between -N and N
+ Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.*/
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -Math.Abs(N);
+while (count <= Math.Abs(N))
+{
+    Console.Write(count + " ");
+    count++;
+
+}
