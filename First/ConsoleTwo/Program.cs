a58a8f31db142830e6e12b065769966078bfda6e
Smusss <lilly_dallas@bk.ru>
2022-10-17 19:22:28 +0300

    Correct ConsoleTwo

--- First/ConsoleTwo/Program.cs
+++ First/ConsoleTwo/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Square check 
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго*/
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+Console.Write("Число " + a + " это квадрат числа " + b);
+}
+else
+{
+Console.Write("Число " + a + " не является квадратом числа " + b);
+}
