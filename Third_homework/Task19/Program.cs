523a5c4f990d26dc20431194d39d96479d4fd991
Smusss <artem.zemlyanukhin@gmail.com>
2022-11-04 18:08:53 +0300

    Create Third_home work and add Task 19

---
+++ Third_homework/Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет.12821 -> да, 23432 -> да */
+
+// Метод определения палиндрома
+string GetPalindrome(int x)
+{
+    string palindrome = "No, it is not palindrome";
+    if ((Math.Abs(x) / 10000) == (Math.Abs(x) % 10) && (Math.Abs(x) / 1000 % 10) == (Math.Abs(x) / 10 % 10))
+    {
+        palindrome = "Yes, it is palindrome";
+    }
+    return palindrome;
+}
+
+Console.Write("Введите пятизначное число = ");
+int x = Convert.ToInt32(Console.ReadLine());
+if ((Math.Abs(x) > 99999) || (Math.Abs(x) < 10000))
+{
+    Console.WriteLine("Число не соответствует условиям, попробуйте еще раз");
+}
+else
+{
+    string answer = GetPalindrome(x);
+    Console.WriteLine(answer);
+}
