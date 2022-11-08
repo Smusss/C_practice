67d7bab4cf135ad54688146fdd22da1f9797981e
Smusss <lilly_dallas@bk.ru>
2022-11-08 12:19:13 +0300

    Correct First ConsoleFive

--- First/ConsoleFive/Program.cs
+++ First/ConsoleFive/Program.cs
@@ -2,14 +2,15 @@
 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.*/
 
 Console.Write("Введите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-string num = Console.ReadLine();
-var last = 
+int input = Convert.ToInt32(Console.ReadLine());
+int number = Math.Abs(input);
+int last = 0;
 if (number > 999 | number < 100)
 {
     Console.WriteLine("Значение не соответствет запросу, попробуйте снова.");
 }
 else
 {
-     Console.WriteLine(inputLine.last(num));
+    last = number % 10;
+    Console.WriteLine($"Последняя цифра числа {input} это {last}");
 }
