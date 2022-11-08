8b653585d9012f856120687cd2855a18620e7279
Smusss <lilly_dallas@bk.ru>
2022-11-08 17:06:19 +0300

    Add Task23

--- Third_homework/Task23/Program.cs
+++ Third_homework/Task23/Program.cs
@@ -2,4 +2,19 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125 */
 
-Console.WriteLine("Hello, World!");
+void PrintCube(int N)
+{
+    int i = 0;
+    for (i = 1; i <= N; i++)
+        Console.Write(i * i * i + " ");
+}
+Console.Write("Введите число N (больше нуля): ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+{
+    Console.Write("Число не соответствует параметру. Попробуйте снова");
+}
+else
+{
+    PrintCube(N);
+}
