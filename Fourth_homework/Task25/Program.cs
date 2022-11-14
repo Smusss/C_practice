82c101658c4d6a1b2e61bf9bf4ac2365fb31bdf3
Smusss <lilly_dallas@bk.ru>
2022-11-14 19:58:04 +0300

    Correct Task25 - add  function

--- Fourth_homework/Task25/Program.cs
+++ Fourth_homework/Task25/Program.cs
@@ -1,48 +1,39 @@
 ﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
 // Для решения задания использование цикла for является обязательным условием. Не использовать встроенный метод возведения в степень.
 // 3, 5 -> 243 (3⁵)
-bool check = false;
-string As = String.Empty;
-string Bs = String.Empty;
 
-do
+int ReadAndCheckSymbol(string message)
 {
-    Console.Write("Введите число A: ");
-    As = Console.ReadLine();
-    check = Double.TryParse(As, out double Ad);
-    if (check == false)
+    bool check = false;
+    string? numberS = String.Empty;
+    int numberI = 0;
+    do
     {
-       Console.WriteLine("Введено неверное значение, попробуйте еще раз.");
+        Console.Write(message);
+        numberS = Console.ReadLine();
+        check = Int32.TryParse(numberS, out numberI);
+        if (!check)
+        {
+            Console.WriteLine("Введено неверное значение, попробуйте еще раз.");
+        }
     }
+    while (!check);
+    return numberI;
 }
-while (check == false);
-//double A = Convert.ToDouble(Console.ReadLine());
 
-do
-{
-    Console.Write("Введите число B: ");
-    Bs = Console.ReadLine();
-    check = Double.TryParse(Bs, out double Bd);
-    if (check == false)
-    {
-        Console.WriteLine("Введено неверное значение, попробуйте еще раз.");
-    }
-}
-while (check == false);
-//double B = Convert.ToDouble(Console.ReadLine());
-
-double A = Convert.ToDouble(As);
-double B = Convert.ToDouble(Bs);
+string message = "Введите число A: ";
+int A = ReadAndCheckSymbol(message);
+message = "Введите число B: ";
+int B = ReadAndCheckSymbol(message);
 
 double PowA = 1;
-int i = 1;
 if (B == 0)
 {
     Console.WriteLine($"Число {A} в степени {B} равно {PowA}");
 }
 else
 {
-    for (i = 1; i <= Math.Abs(B); i++)
+    for (int i = 0; i < Math.Abs(B); i++)
     {
         PowA = PowA * A;
     }
