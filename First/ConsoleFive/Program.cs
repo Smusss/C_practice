c2b9415d76abb01b090bd9fefc896da1ef46f248
Elena Smus <lilly_dallas@bk.ru>
2022-11-14 15:37:45 +0300

    Correct First Console 5h

--- First/ConsoleFive/Program.cs
+++ First/ConsoleFive/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*The last number in three-digit number
-Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.*/
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 
 Console.Write("Введите трехзначное число: ");
 int input = Convert.ToInt32(Console.ReadLine());
@@ -11,6 +11,5 @@ if (number > 999 | number < 100)
 }
 else
 {
-    last = number % 10;
-    Console.WriteLine($"Последняя цифра числа {input} это {last}");
-}
+     Console.WriteLine(inputLine.last(num));
+} */
