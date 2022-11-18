50e274657142be097adc2002e1eed10ca3a961d2
Smusss <lilly_dallas@bk.ru>
2022-11-18 11:18:56 +0300

    Add Console5Two

---
+++ Five/Console5Two/Program.cs
@@ -0,0 +1,22 @@
+﻿//замена элементов с положительных на отриц и наоборот
+
+void showNumbers (int[] array)
+{
+  for (int i = 0;i < array.Length; i++)
+    {
+        Console.Write(array[i] + " "); 
+}
+}
+Console.Write("Исходный массив: ");
+int [] array = new int [8] {0, 1, 2, 3, 4, -1, -2, -3};
+
+for (int i = 0;i < array.Length; i++)
+{
+    {
+        Console.Write(array[i] + " ");
+        array[i] = array[i] * -1;
+    }
+}
+Console.WriteLine("");
+Console.Write("Откорректированный массив: ");
+showNumbers(array);
