029214bf312795403843df59f4556b77ad9a3d8f
Smusss <lilly_dallas@bk.ru>
2022-11-21 19:00:20 +0300

    Add task412

---
+++ Sixth/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// преобразовать число из десятичной в двоичную. Обязательно через метод.
+
+
+string GetBinary (int number)
+{ 
+string binary = string.Empty;
+while (number > 0)
+{
+   binary = Convert.ToString(number % 2) + binary;
+   number = number / 2;
+}
+return binary;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string binary = GetBinary(number);
+Console.Write($"Число в двоичной системе: {binary}");
