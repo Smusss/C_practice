367cfef5bbf308cefeb5ea6ce6866ecb860aa917
Smusss <lilly_dallas@bk.ru>
2022-11-21 19:54:23 +0300

    Add task44

---
+++ Sixth/Task44/Program.cs
@@ -0,0 +1,15 @@
+﻿// вывести первые N чисел Фибоначчи без использования рекурсии и метода
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+array[0] = 0;
+array[1] = 1;
+for (int i = 2; i < n; i++)
+{
+    array[i] = array[i-1] + array[i-2];
+}
+ Console.WriteLine();
+for (int i = 0; i < n;i++)
+{
+    Console.Write(array[i] + " ");
+}
