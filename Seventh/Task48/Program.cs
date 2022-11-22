1ac2bd25e787250b626f3a484872960ae4267ddd
Smusss <lilly_dallas@bk.ru>
2022-11-22 20:17:51 +0300

    correct task 48

---
+++ Seventh/Task48/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+
+Console.Write("Input m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] table = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        table [i, j] = i + j;
+    }
+}
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(table[i, j] + " ");
+    }
+    Console.WriteLine();
+}
