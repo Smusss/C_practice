4783875ce8ee7b1dd8ef9d93805f26cd9e1c6401
Smusss <lilly_dallas@bk.ru>
2022-11-22 23:21:01 +0300

    correct task 47

--- Seventh_homework/Task47/Program.cs
+++ Seventh_homework/Task47/Program.cs
@@ -21,7 +21,7 @@ double[,] FillTableRandomNumbers(int m, int n)
 }
 void PrintTable(double[,] table)
 {
-    for (int i = 0; i < table.GetLength(0) + 1; i++)
+    for (int i = 0; i < table.GetLength(0); i++)
     {
         for (int j = 0; j < table.GetLength(1); j++)
         {
