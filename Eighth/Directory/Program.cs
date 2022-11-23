69a6e672bfcc364ec9ec962e3a467ebeb95fa7a5
Smusss <lilly_dallas@bk.ru>
2022-11-23 20:12:05 +0300

    Add Eight (recursion)

---
+++ Eighth/Directory/Program.cs
@@ -0,0 +1,17 @@
+﻿// просмотр папки
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalogs = new DirectoryInfo(path);
+    foreach (var currentCatalog in catalogs.GetDirectories())
+    {
+        Console.WriteLine($"{indent}{currentCatalog.Name}");
+        CatalogInfo(currentCatalog.FullName, indent + " ");
+    }
+    foreach (var item in catalogs.GetFiles())
+    {
+        Console.WriteLine($"{indent}{item.Name}");
+    }
+}
+Console.Write("Input directory adress: ");
+string path = Console.ReadLine();
+CatalogInfo(path);
---
+++ Eighth/Tower/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+ if (count > 1) Towers(with, some, on, count - 1);
+ Console.WriteLine($"{with} >> {on}");
+ if (count > 1) Towers(some, on, with, count - 1);
+}
