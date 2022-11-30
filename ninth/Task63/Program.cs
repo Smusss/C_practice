22bb05a8ed9ee936e42bceca37665f8ca2364cd2
Smusss <lilly_dallas@bk.ru>
2022-11-30 17:34:22 +0300

    add task 63

---
+++ ninth/Task63/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет  все натуральные числа в промежутке от N до 1.
+int Numbers(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int result = Numbers(n - 1);
+    Console.Write(n + " ");
+    return result;
+}
+
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Num = Numbers(n);
