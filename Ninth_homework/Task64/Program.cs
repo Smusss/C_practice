f25bb72c432b83834acf410df0431f896ab20fde
Smusss <lilly_dallas@bk.ru>
2022-11-30 17:35:15 +0300

    add task 64 and 66

---
+++ Ninth_homework/Task64/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+int NumbersBetween(int m, int n)
+{
+    Console.Write(m + " ");
+    if (m == n)
+    {
+        return n;
+    }
+    return NumbersBetween(m + 1, n);
+}
+
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < m)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+int Num = NumbersBetween(m,n);
+
+Console.WriteLine();
+Console.WriteLine();
+
+void Between(int k, int l)
+{
+    Console.Write(k + " ");
+    if (k == l)
+    {
+        return;
+    }
+   NumbersBetween(k + 1, l);
+}
+Console.Write("Input K: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input L: ");
+int l = Convert.ToInt32(Console.ReadLine());
+if (l < k)
+{
+    int temp = l;
+    l = k;
+    k = temp;
+}
+Between(k,l);
---
+++ Ninth_homework/Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumBetween(int m, int n)
+{
+    if (n - m <= 1)
+    {
+        return 0;
+    }
+    return m + 1 + SumBetween(m + 1, n);
+}
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < m)
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+int sum = SumBetween(m, n);
+Console.Write($"Sum elements between= {sum}");
